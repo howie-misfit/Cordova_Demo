df1f959d2eb70902d8e80724908919fdd2f3b63e
howie-misfit <howie@misfit.com>
2016-07-01 18:47:52 +0800

    Define and use template.

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -2,47 +2,41 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
-        renderHomeView();
+      renderHomeView();
     });
 
-    /* --------------------------------- Event Registration -------------------------------- */
-    document.addEventListener('deviceready', function () {
-        FastClick.attach(document.body);
-        if (navigator.notification) { // Override default HTML alert with native dialog
-            window.alert = function (message) {
-                navigator.notification.alert(
-                    message,    // message
-                    null,       // callback
-                    "Workshop", // title
-                    'OK'        // buttonName
-                );
-            };
-        }
-    }, false);
 
+    /* --------------------------------- Event Registration -------------------------------- */
+
+    document.addEventListener('deviceready', function () {
+
+      FastClick.attach(document.body);
+      if (navigator.notification) { // Override default HTML alert with native dialog
+          window.alert = function (message) {
+              navigator.notification.alert(
+                  message,    // message
+                  null,       // callback
+                  "Workshop", // title
+                  'OK'        // buttonName
+              );
+          };
+      }
+    }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.content').html(employeeListTpl(employees));
         });
     }
 
     function renderHomeView() {
-        var html =
-            "<h1>Directory</h1>" +
-                "<input class='search-key' type='search' placeholder='Enter name'/>" +
-                "<ul class='employee-list'></ul>";
-        $('body').html(html);
-        $('.search-key').on('keyup', findByName);
+       $('body').html(homeTpl());
+       $('.search-key').on('keyup', findByName);
     }
 
 
