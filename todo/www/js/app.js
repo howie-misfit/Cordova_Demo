b74f9233e2769b812d0b3f2b89cf98e5b92f19c0
howie-misfit <howie@misfit.com>
2016-07-05 11:37:46 +0800

    Refactor with Home View.

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -2,44 +2,32 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-    var homeTpl = Handlebars.compile($("#home-tpl").html());
-    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+
     var service = new EmployeeService();
     service.initialize().done(function () {
-      renderHomeView();
+        $('body').html(new HomeView(service).render().$el);
     });
 
-
     /* --------------------------------- Event Registration -------------------------------- */
-
     document.addEventListener('deviceready', function () {
-      StatusBar.overlaysWebView( false );
-      StatusBar.backgroundColorByHexString('#ffffff');
-      StatusBar.styleDefault();
-      FastClick.attach(document.body);
-      if (navigator.notification) { // Override default HTML alert with native dialog
-          window.alert = function (message) {
-              navigator.notification.alert(
-                  message,    // message
-                  null,       // callback
-                  "Workshop", // title
-                  'OK'        // buttonName
-              );
-          };
-      }
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
     }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            $('.content').html(employeeListTpl(employees));
-        });
-    }
-
-    function renderHomeView() {
-       $('body').html(homeTpl());
-       $('.search-key').on('keyup', findByName);
-    }
-
 
 }());
